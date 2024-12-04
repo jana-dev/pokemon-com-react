532376623f250b774d5530b17c9a2780e430aa6c
Janaina Tavares <52297249+jana-dev@users.noreply.github.com>
2024-12-04 15:18:41 -0300

    Update pokemoncard
    Dados consumidos da api e validações de cores para atributos e tipos

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,10 +2,24 @@ import imgPokemon from '../assets/Bulbasaur.png'
 
 function PokemonCard({pokemonData}) {
 
+
   if(!pokemonData){
     return <p>Nenhum Pokemon encontrado.</p>
   }
 
+  const stats = pokemonData.stats
+  const hp = stats[0].base_stat
+  const attack = stats[1].base_stat
+  const defense = stats[2].base_stat
+  const speed = stats[5].base_stat
+
+  const getStatsPokemon = (value) => {
+    if (value > 50) return "valor-atributo-maior"
+    if (value < 50) return "valor-atributo-menor"
+    return ""
+  }
+
+  const getTypePokemon = (value) => `valor-tipo ${value}`
 
   return (
     <div className="pokemon-card">
@@ -13,26 +27,26 @@ function PokemonCard({pokemonData}) {
       <div className="tipo">
         <h2>{pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</h2>
         {pokemonData.types.map((type, index) => (
-          <p key={index} className='valor-tipo'>{type.type.name}</p>
+          <p key={index} className={getTypePokemon(type.type.name)}>{type.type.name}</p>
         ))}
      
       </div>
       <div className="atributos">
         <div>
           <p className='nome-atributo'>HP</p>
-          <p>89</p>
+          <p className={getStatsPokemon(hp)}>{hp}</p>
         </div>
         <div>
           <p className='nome-atributo'>Ataque</p>
-          <p>66</p>
+          <p className={getStatsPokemon(attack)}>{attack}</p>
         </div>
         <div>
           <p className='nome-atributo'>Defesa</p>
-          <p>32</p>
+          <p className={getStatsPokemon(defense)}>{defense}</p>
         </div>
         <div>
           <p className='nome-atributo'>Velocidade</p>
-          <p>45</p>
+          <p className={getStatsPokemon(speed)}>{speed}</p>
         </div>
       </div>
     </div>
